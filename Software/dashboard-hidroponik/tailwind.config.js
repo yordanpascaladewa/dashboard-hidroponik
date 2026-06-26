2165019eacc98dc7e218fd248dfbe5e14afd212b
Yordan Pascaladewa <yordanpascaladewaa@gmail.com>
2026-06-27 03:59:17 +0700

    p

--- Software/dashboard-hidroponik/tailwind.config.js
+++ Software/dashboard-hidroponik/tailwind.config.js
@@ -1,12 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}", // INI YANG PALING PENTING
+    "./src/**/*.{js,ts,jsx,tsx,mdx}", // Ini akan menyapu bersih semua file di dalam src
+    "./app/**/*.{js,ts,jsx,tsx,mdx}", // Jaga-jaga kalau Next.js membaca folder app
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-};
+}
