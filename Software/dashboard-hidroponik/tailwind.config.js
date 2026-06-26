2d8e0e4e0e25611efbefd6e50c4968fd8ac5ecc2
Yordan Pascaladewa <yordanpascaladewaa@gmail.com>
2026-06-26 14:21:02 +0700

    p

---
+++ Software/dashboard-hidroponik/tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#10b981", // Emerald 500
+        surface: "#f7f9fb",
+      },
+    },
+  },
+  plugins: [],
+};
