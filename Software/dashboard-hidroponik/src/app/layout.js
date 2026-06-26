4e367024125f04ad4d98bf37afbf4743324e2675
Yordan Pascaladewa <yordanpascaladewaa@gmail.com>
2026-06-27 03:52:47 +0700

    p

--- Software/dashboard-hidroponik/src/app/layout.js
+++ Software/dashboard-hidroponik/src/app/layout.js
@@ -1,19 +1,17 @@
 import { Inter } from 'next/font/google';
-import './globals.css';
+import './globals.css'; // Ini sekarang bakal dibaca oleh Next.js!
 
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
-  title: 'AeroGrow Pro - System',
-  description: 'Precision DFT Telemetry & Embedded Data Management',
+  title: 'AeroGrow Pro',
+  description: 'Precision DFT Telemetry System',
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="id">
-      <body className={inter.className}>
-        {children}
-      </body>
+      <body className={inter.className}>{children}</body>
     </html>
   );
 }
